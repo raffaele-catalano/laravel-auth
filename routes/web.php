7947a91dcd70ae3eef7caffcaf1d96763fa00902
raffaele-catalano <raf.catalano@gmail.com>
2023-06-15 17:48:04 +0200

    created Route:middleware for Admin + filled admin.home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('home');
+    });
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
