dfc5cab0ed57a950e0aadd67d7edeb2f1e3d886f
raffaele-catalano <raf.catalano@gmail.com>
2023-06-16 22:28:38 +0200

    completed Delete/Destroy resource

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -102,8 +102,10 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('admin.projects.index')->with('deleted', "The Project -> $project->name has been succesfully deleted");
     }
 }
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,35 +2,53 @@
 
 @section('content')
     <h3 class="text-center mt-3">Projects</h3>
-        <div class="container d-flex flex-column align-items-center">
-            <table class="table table-striped table-hover w-75">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ( $projects as $project )
+    <div class="container d-flex flex-column align-items-center">
+
+        @if (session('deleted'))
+            <div class="alert alert-success" role="alert">
+                {{ session('deleted') }}
+            </div>
+        @endif
+
+        <table class="table table-striped table-hover w-75">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
                     <tr>
                         <td>{{ $project->id }}</td>
                         <td>{{ $project->name }}</td>
                         <td class="text-capitalize">{{ $project->category }}</td>
                         <td>{{ $project->is_closed ? 'Closed' : 'Ongoing' }}</td>
                         <td>
-                            <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-primary"><i class="fa-solid fa-eye fa-lg"></i></a>
-                            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning"><i class="fa-solid fa-pencil fa-lg"></i></a>
-                            <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash-can fa-lg"></i></a>
+                            <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-primary"><i
+                                    class="fa-solid fa-eye fa-lg"></i></a>
+                            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning"><i
+                                    class="fa-solid fa-pencil fa-lg"></i></a>
+                            <form action="{{ route('admin.projects.destroy', $project) }}" method="POST"
+                                class="d-inline ms-1"
+                                onsubmit="return confirm('The project -> {{ $project->name }} will be deleted, are you sure?')">
+
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit" class="btn btn-danger fw-bold"><i
+                                        class="fa-solid fa-trash-can fa-lg"></i></button>
+                            </form>
                         </td>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div>
-                {{ $projects->links() }}
-            </div>
+                @endforeach
+            </tbody>
+        </table>
+        <div>
+            {{ $projects->links() }}
         </div>
+    </div>
 @endsection
