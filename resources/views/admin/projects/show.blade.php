8f4a883aded3c79c9fdc30db5f6feaa3649bede1
raffaele-catalano <raf.catalano@gmail.com>
2023-06-19 16:38:43 +0200

    implemented in Show the possibility to Edit&Delete

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,12 +1,29 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h3 class="text-center mt-3">{{ $project->name }}</h3>
-        <div class="container d-flex flex-column align-items-center">
-            <h5><span class="text-primary">Category:</span> {{ $project->category }}</h5>
-            <h5><span class="text-primary">Start Date:</span> {{ $project->start_date }}</h5>
-            <h5><span class="text-primary">End Date:</span> {{ $project->end_date }}</h5>
-            <h5><span class="text-primary">Status:</span> {{ $project->is_closed ? 'Closed' : 'Ongoing' }}</h5>
-            <p>{{ $project->description }}</p>
-        </div>
+
+    <div class="d-flex align-items-center justify-content-center">
+        <h3 class="text-center mt-3">{{ $project->name }}</h3>
+        <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning mx-2"><i
+            class="fa-solid fa-pencil fa-lg"></i></a>
+
+        @include('admin.partials.delete-form')
+    </div>
+
+    <div class="container d-flex justify-content-center mb-2">
+        <img src="{{ asset('storage/' . $project->image_path) }}" alt="" class="w-50">
+    </div>
+    <div class="container d-flex flex-column align-items-center">
+        <h5><span class="text-primary">Category:</span> {{ $project->category }}</h5>
+        <h5><span class="text-primary">Start Date:</span> {{ $project->start_date }}</h5>
+        <h5><span class="text-primary">End Date:</span> {{ $project->end_date }}</h5>
+        <h5><span class="text-primary">Status:</span> {{ $project->is_closed ? 'Closed' : 'Ongoing' }}</h5>
+        <p>{!! $project->description !!}</p>
+    </div>
+    <div class="container d-flex justify-content-center">
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-primary mx-2">
+            Back to Dashboard
+        </a>
+    </div>
+
 @endsection
