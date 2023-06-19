8918a5245ef3912a6b603eaa96c6ec71b533a0f1
raffaele-catalano <raf.catalano@gmail.com>
2023-06-19 18:47:31 +0200

    fixed update migration&rollback

--- database/migrations/2023_06_19_075641_update_projects_table.php
+++ database/migrations/2023_06_19_075641_update_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             $table->string('image_path')->nullable()->after('name');
-            $table->string('image_original_name')->nullable()->after('name');
+            $table->string('image_original_name')->nullable()->after('image_path');
         });
     }
 
