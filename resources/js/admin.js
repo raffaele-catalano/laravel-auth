322026cd5425c1e25652bba03172ef083fc3a09b
raffaele-catalano <raf.catalano@gmail.com>
2023-06-19 12:45:13 +0200

    fixed import of scss file

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,6 +1,8 @@
 import './bootstrap';
-import '~resources/scss/app.scss';
+import '~resources/scss/admin.scss';
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+console.log('PROVA LOG');
